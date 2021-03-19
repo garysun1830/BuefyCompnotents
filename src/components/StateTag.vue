f8c45c2ad83eeb1811aa265b66dee1a2cddd02df
Gary Sun <jelly1830@gmail.com>
2021-03-18 22:06:54 -0700

    add more components

---
+++ src/components/StateTag.vue
@@ -0,0 +1,55 @@
+<template>
+    <section>
+        <b-field
+            label="Success"
+            type="is-success">
+            <b-taginput
+                :value="['Tag']">
+            </b-taginput>
+        </b-field>
+
+        <b-field
+            label="Error"
+            type="is-danger">
+            <b-taginput
+                :value="['Tag']">
+            </b-taginput>
+        </b-field>
+
+        <b-field
+            label="Info"
+            type="is-info">
+            <b-taginput
+                :value="['Tag']">
+            </b-taginput>
+        </b-field>
+
+        <b-field
+            label="Warning"
+            type="is-warning">
+            <b-taginput
+                :value="['Tag']">
+            </b-taginput>
+        </b-field>
+
+        <b-field label="Disabled">
+            <b-taginput
+                :value="['Tag']"
+                disabled>
+            </b-taginput>
+        </b-field>
+
+        <b-field label="Loading">
+            <b-taginput
+                :value="['Tag']"
+                loading>
+            </b-taginput>
+        </b-field>
+    </section>
+</template>
+
+<script>
+export default {
+    name:"StateTag",
+}
+</script>
